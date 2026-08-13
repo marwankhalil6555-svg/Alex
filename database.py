ba185bcaf81ea0752616e6962fc635fcf894fb7d
marwankhalil6555-svg <marwankhalil6555@gmail.com>
2026-08-13 14:53:14 +0300

    Refactor command lists for better readability

--- database.py
+++ database.py
@@ -1,29 +1,79 @@
-music = ["music"]
-browser = ["open browser","open chrome"]
-chat = ["how are you","how is it going"]
-greetings = ["hi","hello","welcome","good morning"]
-explorer = ["open files","files","windows explorer","explorer","explore"]
-interests = ["i like","i love","i prefer","iam intrested"]
-questions = ["you like","you love","you want","you prefer","do you intrested","you intrested of","do you like","do you love"]
-exit_commands = ["exit","quit","goodbye"]
-notepad = ["notepad","note"]
-randomise = ["choose","random"]
-agent_id = ["what's your name","what is your name"]
-check = ["still working","still there","there"]
-browse = ["browse","search","visit","visite"]
-websites = ["facebook","youtube","instagram","whatsapp","reddit","wikipedia","google","disocrd","amazone","twitter"]
-vol_up = ["volume up","sound up","vol up","volumeup","vloup","volum up","volumup","louder","too soft","to soft","higher"]
-vol_down = ["volume down","sound down","voldown","volumedown","volum down","volumdown","softer","too loud","to loud","lower"]
-mute = ["mute","volume off","sound off","volume of","sound of"]
-terminal = ["terminal","cmd","powershell","command line"]
-thanks = ["thank you","thankyou","thank's","thanks"]
-valorant = ["valorant"]
-close = ["shutdown","turn off pc","turn off computer","shut down","turn off my pc"]
-accept = ["yes","yah","ok"]
-code = ["vscode","vs code","visual studio code","code editor","code","coder"]
-time_ask = ["what time is it","time","oclock"]
-server = ["nas","server"]
-yt = ["youtube","online"]
-close_tab = ["close tab","tab off","kill tab","killtab","closetab","close page","page off"]
-close_music = ["stop","music off"]
-close_browser = ["chrome off","close chrome","close browser","browser off","kill browser","killbrowser"]
+music = ["music", "play music", "play a song", "start music", "play some music", "audio"]
+
+browser = ["open browser", "open chrome", "launch browser", "start chrome", "open google chrome", "browser"]
+
+chat = ["how are you", "how is it going", "how are you doing", "how have you been", "what's up", "how's it going"]
+
+greetings = ["hi", "hello", "welcome", "good morning", "good evening", "good afternoon", "hey", "hey there"]
+
+explorer = ["open files", "files", "windows explorer", "explorer", "explore", "file manager", "open explorer", "my computer"]
+
+interests = ["i like", "i love", "i prefer", "iam intrested", "i am interested in", "i enjoy", "i really like"]
+
+questions = [
+    "you like", "you love", "you want", "you prefer", "do you intrested", "you intrested of", 
+    "do you like", "do you love", "are you interested in", "do you enjoy", "what do you like"
+]
+
+exit_commands = ["exit", "quit", "goodbye", "bye", "stop script", "close assistant", "leave"]
+
+notepad = ["notepad", "note", "open notepad", "take a note", "open notes", "notes"]
+
+randomise = ["choose", "random", "pick one", "randomize", "pick for me", "select random", "anything", "any thing", "any"]
+
+agent_id = ["what's your name", "what is your name", "who are you", "tell me your name", "your name"]
+
+check = ["still working", "still there", "there", "are you there", "are you working", "you still there"]
+
+browse = ["browse", "search", "visit", "visite", "look up", "search for", "google"]
+
+websites = [
+    "facebook", "youtube", "instagram", "whatsapp", "reddit", "wikipedia", 
+    "google", "disocrd", "discord", "amazone", "amazon", "twitter", "pinterest", "x"
+]
+
+vol_up = [
+    "volume up", "sound up", "vol up", "volumeup", "vloup", "volum up", "volumup", 
+    "louder", "too soft", "to soft", "higher", "volumov", "increase volume", "turn it up"
+]
+
+vol_down = [
+    "volume down", "sound down", "voldown", "volumedown", "volum down", "volumdown", 
+    "softer", "too loud", "to loud", "lower", "voliumdown", "decrease volume", "turn it down"
+]
+
+mute = ["mute", "volume off", "sound off", "volume of", "sound of", "silence"]
+
+terminal = ["terminal", "cmd", "powershell", "command line", "open terminal", "command prompt"]
+
+thanks = ["thank you", "thankyou", "thank's", "thanks", "thanks a lot", "much appreciated"]
+
+valorant = ["valorant", "play valorant", "launch valorant", "open valorant"]
+
+close = ["shutdown", "turn off pc", "turn off computer", "shut down", "turn off my pc", "shutdown computer"]
+
+accept = ["yes", "yah", "ok", "yeah", "yep", "sure", "confirm"]
+
+code = ["vscode", "vs code", "visual studio code", "code editor", "code", "coder", "open vscode", "open code"]
+
+time_ask = ["what time is it", "time", "oclock", "what's the time", "current time", "tell me the time"]
+
+server = ["nas", "server", "open server", "connect to server", "home server"]
+
+yt = ["youtube", "online", "open youtube", "yt"]
+
+close_tab = ["close tab", "tab off", "kill tab", "killtab", "closetab", "close page", "page off", "exit tab"]
+
+close_music = ["stop", "music off", "turn off music", "stop music", "pause music", "stop audio"]
+
+close_browser = ["chrome off", "close chrome", "close browser", "browser off", "kill browser", "killbrowser", "exit browser"]
+
+clapy_mode = ["clap mode", "remote" ,"clapy" ,"clapy mode", "clapy on", "clap on", "clap mode on"]
+
+clapy_mode_off = ["clapy off", "clapy mode off", "clap mode off", "clap off", "remote off"]
+
+sleeping_mode = ["sleep", "sleeping mode", "take a rest", "alex sleep", "alex sleeping mode", "sleep on"]
+
+chill_mode = ["chill mode on", "chill mode", "relax mode on", "relax", "relax mode", "chilling mode on"]
+
+movies = ["netflex", "tv", "movies", "jellyfin", "movie"]
